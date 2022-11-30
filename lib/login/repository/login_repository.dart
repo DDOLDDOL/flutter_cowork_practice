52c21d9b529d37c3bebe265537e4b026c020ac1b
DDOLDDOL <doyt@naver.com>
2022-11-30 21:10:08 +0900

    Revert "Revert "chore: Add packages""

---
+++ lib/login/repository/login_repository.dart
@@ -0,0 +1,9 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:graphql_music_1116/config.dart';
+
+
+class LoginRepository {
+  final Config client;
+  const LoginRepository(this.client);
+
+}
