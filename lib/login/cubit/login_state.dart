16055a5e84caf79cd03b57ebf483e14afe87f901
leejeongsoo1660 <74721370+leejeongsoo1660@users.noreply.github.com>
2022-12-05 15:02:58 +0900

    refactor: Fix login state

--- lib/login/cubit/login_state.dart
+++ lib/login/cubit/login_state.dart
@@ -1,6 +1,6 @@
 part of 'login_cubit.dart';
 
-abstract class LoginState extends Equatable {
+abstract class LoginState {
   const LoginState();
 
   @override
@@ -9,27 +9,28 @@ abstract class LoginState extends Equatable {
 
 class LoginInitial extends LoginState {}
 
-class LoginLoading extends LoginState {}
-
-class   LoginPassword extends LoginState {
-  final int userId;
-  const LoginPassword(this.userId);
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is LoginPassword && other.userId == userId;
-  }
-
-  @override
-  int get hashCode => userId.hashCode;
+class LoginLoaded extends LoginState {
+  // final String userId;
+  // const LoginLoaded(this.userId);
+  //
+  // @override
+  // bool operator ==(Object other) {
+  //   if (identical(this, other)) return true;
+  //
+  //   return other is LoginLoaded && other.userId == userId;
+  // }
+  // @override
+  // int get hashCode => userId.hashCode;
 }
 
+class LoginLoading extends LoginState {}
+
 class LoginSuccess extends LoginState {
+  final String userId;
   final String accessToken;
   final String refreshToken;
-  const LoginSuccess(this.accessToken, this.refreshToken);
+  const LoginSuccess(this.userId,this.accessToken, this.refreshToken);
+
 
   @override
   int get hashCode => accessToken.hashCode ^ refreshToken.hashCode;
